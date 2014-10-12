ed0ef855f90c7cb9bd12ad871e12c59edac69167
Michael Bergens <mubergens@gmail.com>
2014-10-12 14:06:23 -0700

    Issue #1: simplified the code, eliminated a class

---
+++ jetBrains/idea/plugins/copyNixPath/src/org/momuniverse42/idea/copyNixPath/CopyNixDirPath.java
@@ -0,0 +1,20 @@
+package org.momuniverse42.idea.copyNixPath;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.io.File;
+
+/**
+ * @author mubergens Michael Bergens
+ */
+public class CopyNixDirPath extends CopyNixFilePath {
+
+    /**
+     * Snips off the filename, returns the file's directory only.
+     */
+    @Override @NotNull public String adjust(final @NotNull String path) {
+        final File f = new File(path);
+
+        return f.isDirectory() ? path : f.getParent();
+    }
+}
