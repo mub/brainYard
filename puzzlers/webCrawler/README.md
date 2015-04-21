3c4a323df3a0ee9e5795c77b5da206af8819d8dd
Michael Bergens <michael.bergens+github@gmail.com>
2015-04-21 13:38:05 -0700

    WebCrawler challenge stub creation
    Created the stub

---
+++ puzzlers/webCrawler/README.md
@@ -0,0 +1,3 @@
+# Worldcrawler challenge project
+
+Work in progress
---
+++ puzzlers/webCrawler/src/test/java/test/github/mub/webCrawler/TestWc.java
@@ -0,0 +1,20 @@
+package test.github.mub.webCrawler;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.slf4j.Logger;
+
+import java.util.List;
+
+import static org.slf4j.LoggerFactory.getLogger;
+
+/**
+ */
+public class TestWc {
+    private static final Logger L = getLogger(TestWc.class);
+
+    @Before public void init() throws Exception { } @After public void destroy() throws Exception { }
+
+
+}
